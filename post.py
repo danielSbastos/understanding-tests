d712467998a6723e36f2bde2294cb47e81df5957
Daniel Bastos <daniel.bastos1710@gmail.com>
2017-12-13 17:33:48 -0200

    Rename variable and fix typo

--- post.py
+++ post.py
@@ -3,14 +3,14 @@ import requests
 
 
 class Post():
-    MINIMUM_LENGHT = 100
+    MINIMUM_LENGTH = 100
 
     def __init__(self, text):
         self.text = text
         self.has_accepted_size = self.has_minumum_size((len(self.text)))
 
-    def has_minumum_size(self, length):
-        return length > self.MINIMUM_LENGHT
+    def has_minumum_size(self, text_length):
+        return text_length > self.MINIMUM_LENGTH
 
     def publish_post(self):
         if self.has_accepted_size:
@@ -19,3 +19,4 @@ class Post():
                 headers={'content-type': 'application/json'},
                 data=json.dumps({'text': self.text})
             )
+
